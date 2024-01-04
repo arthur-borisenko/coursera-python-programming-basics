b3750385bf14979126a8fbc809a79bbc632517a6
arthur-borisenko <artour.borisenko@yandex.ru>
2024-01-04 23:23:57 +0300

    refactored to use iterator

--- week2/36-kolichiestvo-eliemientov-bol-shie-priedydushchiegho/task.py
+++ week2/36-kolichiestvo-eliemientov-bol-shie-priedydushchiegho/task.py
@@ -1,11 +1,28 @@
+class InputIntStream:
+    def __init__(self):
+        pass
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        self.current = int(input())
+        if self.current != 0:
+            return self.current
+        raise StopIteration
+
+
 def main():
+    iterator = InputIntStream()
     res = 0
-    number = int(input())
-    while number != 0:
-        prevNumber = number
-        number = int(input())
-        if number > prevNumber and number != 0:
-            res += 1
+    prevNumber = None
+    for number in iterator:
+        if prevNumber is None:
+            prevNumber = number
+        else:
+            if number > prevNumber and number != 0:
+                res += 1
+            prevNumber = number
     print(res)
 
 
