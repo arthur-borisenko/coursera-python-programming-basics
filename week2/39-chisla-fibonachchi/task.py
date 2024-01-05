f69544b684d5107a99ad3ab3ef787477f4dc111a
arthur-borisenko <artour.borisenko@yandex.ru>
2024-01-05 23:26:45 +0300

    refactored

--- week2/39-chisla-fibonachchi/task.py
+++ week2/39-chisla-fibonachchi/task.py
@@ -1,13 +1,14 @@
 def main():
-    n = get_fibonacci_number(int(input()))
-    print(n)
+    n = int(input())
+    Fn = get_fibonacci_number(n)
+    print(Fn)
 
 
-def get_fibonacci_number(N):
+def get_fibonacci_number(n):
     Fn2 = 1
     Fn1 = 0
     Fn = 0
-    for i in range(N):
+    for i in range(n):
         Fn = Fn1 + Fn2
         Fn2 = Fn1
         Fn1 = Fn
