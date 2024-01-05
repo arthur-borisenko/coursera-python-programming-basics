e56bf8c7dd3e26402482b9489bcbf2e0ef83e859
arthur-borisenko <66972198+arthur-borisenko@users.noreply.github.com>
2024-01-05 23:01:20 +0300

    task 38 solved (#14)
    * solved https://www.coursera.org/learn/python-osnovy-programmirovaniya/programming/emyMl/kolichiestvo-eliemientov-ravnykh-maksimumu
    
    * added task image
    
    * adjusted efficiency
    
    * refactored
    
    * refactored
    
    ---------
    
    Co-authored-by: arthur-borisenko <artour.borisenko@yandex.ru>

---
+++ week2/38-kolichiestvo-eliemientov-ravnykh-maksimumu/task.py
@@ -0,0 +1,33 @@
+def count_max_elements(seq):
+    max_count = 0
+    max_el = None
+    for el in seq:
+        if max_el is None or max_el < el:
+            max_count = 0
+            max_el = el
+        if el == max_el:
+            max_count += 1
+    return max_count
+
+
+def main():
+    seq = InputIntStream()
+    print(count_max_elements(seq))
+
+
+class InputIntStream:
+    def __init__(self):
+        pass
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        self.current = int(input())
+        if self.current != 0:
+            return self.current
+        raise StopIteration
+
+
+if __name__ == '__main__':
+    main()
