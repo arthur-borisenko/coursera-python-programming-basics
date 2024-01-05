f402db8feb1ae3cd5fd6eeba3659fdf8f5439e8a
arthur-borisenko <66972198+arthur-borisenko@users.noreply.github.com>
2024-01-05 22:08:27 +0300

    task 37 solved (#13)
    * https://www.coursera.org/learn/python-osnovy-programmirovaniya/programming/4pohA/vtoroi-maksimum/submission
    
    * add task image
    
    * extracted algorythm
    
    * renamed res to max_el
    
    * completed get_top_2_el function
    
    * refactored
    
    ---------
    
    Co-authored-by: arthur-borisenko <artour.borisenko@yandex.ru>

---
+++ week2/37-vtoroi-maksimum/task.py
@@ -0,0 +1,34 @@
+def main():
+    seq = InputIntStream()
+    res = get_top_2_element(seq)
+    print(res)
+
+
+def get_top_2_element(seq):
+    top_2_el = None
+    max_el = None
+    for el in seq:
+        if max_el is None or max_el <= el:
+            top_2_el = max_el
+            max_el = el
+        elif top_2_el is None or top_2_el <= el:
+            top_2_el = el
+    return top_2_el
+
+
+class InputIntStream:
+    def __init__(self):
+        pass
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        self.current = int(input())
+        if self.current != 0:
+            return self.current
+        raise StopIteration
+
+
+if __name__ == '__main__':
+    main()
---
+++ week2/37-vtoroi-maksimum/test.py
@@ -0,0 +1,35 @@
+import unittest
+from os import system
+from task import main
+from utils.testUtil import mockAndRun
+
+
+class Test(unittest.TestCase):
+    def test_case1(self):
+        value = mockAndRun('''1
+7
+9
+0'''.splitlines(), main)
+        expected_result = "7\n"
+        self.assertEqual(expected_result, value)
+
+    def test_case2(self):
+        value = mockAndRun('''2
+1
+0'''.splitlines(), main)
+        expected_result = "1\n"
+        self.assertEqual(expected_result, value)
+
+    def test_case3(self):
+        value = mockAndRun('''1
+2
+2
+3
+3
+0'''.splitlines(), main)
+        expected_result = "3\n"
+        self.assertEqual(expected_result, value)
+    def test_pep8(self):
+        value = system("python -m pycodestyle --first task.py")
+        print(value)
+        self.assertIs(value, 0)
