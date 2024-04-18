ee219c25b379e1372fe2c4185d4639f677292da5
Art <artour.borisenko@ya.ru>
2024-04-18 14:54:24 +0300

    memory usage converted to O(1)

--- week2/46-naimien-shieie-rasstoianiie-miezhdu-lokal-nymi-maksimumami/task.py
+++ week2/46-naimien-shieie-rasstoianiie-miezhdu-lokal-nymi-maksimumami/task.py
@@ -2,46 +2,47 @@ def check_is_local_maximum(prev_n, n, post_n):
     return n > prev_n and n > post_n
 
 
-def get_input_int_sequence():
-    r = []
-    while True:
-        n = int(input())
-        if n != 0:
-            r.append(n)
-        else:
-            return r
+class InputIntStream:
+    def __init__(self):
+        pass
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        self.current = int(input())
+        if self.current != 0:
+            return self.current
+        raise StopIteration
 
 
-def get_local_maximums_positions_list(input_sequence):
-    result = []
-    if len(input_sequence) < 2:
-        return []
-    prev_prev_n = input_sequence[0]
-    prev_n = input_sequence[1]
-    for i in range(2, len(input_sequence)):
-        n = input_sequence[i]
-        if check_is_local_maximum(prev_prev_n, prev_n, n):
-            result.append(i - 1)
-        prev_prev_n = prev_n
-        prev_n = n
-        i += 1
-    return result
+def get_local_maximums_minimal_distance(input_int_stream: InputIntStream):
+    result = None
+    i = 2
+    prev_local_maximum_pos = None
+    try:
+        prev_prev_n = input_int_stream.__next__()
+        prev_n = input_int_stream.__next__()
+    except StopIteration:
+        return 0
+    else:
+        for n in input_int_stream:
+            if check_is_local_maximum(prev_prev_n, prev_n, n):
+                if prev_local_maximum_pos is not None:
+                    if (result is None or i -
+                            prev_local_maximum_pos - 1 <= result):
+                        result = i - prev_local_maximum_pos - 1
+                prev_local_maximum_pos = i - 1
+            prev_prev_n = prev_n
+            prev_n = n
+            i += 1
+        if result is None:
+            return 0
+        return result
 
 
 def main():
-    input_list = get_input_int_sequence()
-    local_maximums_positions_list = \
-        get_local_maximums_positions_list(input_list)
-    if len(local_maximums_positions_list) < 2:
-        print("0")
-    else:
-        res = None
-        for index in range(1, len(local_maximums_positions_list)):
-            if res is None or (local_maximums_positions_list[index] -
-                               local_maximums_positions_list[index - 1] < res):
-                res = (local_maximums_positions_list[index] -
-                       local_maximums_positions_list[index - 1])
-        print(res)
+    print(get_local_maximums_minimal_distance(InputIntStream()))
 
 
 if __name__ == '__main__':
