a1db530923265729f2688c6897caf7d9e7b27bf3
Art <artour.borisenko@ya.ru>
2024-04-28 12:50:01 +0300

    refactored

--- week3/52-summa-riada/task.py
+++ week3/52-summa-riada/task.py
@@ -6,12 +6,12 @@ def if_integer_than_to_int(n):
 
 def main():
     n = int(input())
-    prev_znam = 1
-    prev_div = 1
+    prev_numerator = 1
+    prev_denominator = 1
     for num in range(2, n + 1):
-        prev_znam = prev_znam * num ** 2 + prev_div
-        prev_div *= num ** 2
-    print(if_integer_than_to_int(prev_znam / prev_div))
+        prev_numerator = prev_numerator * num ** 2 + prev_denominator
+        prev_denominator *= num ** 2
+    print(if_integer_than_to_int(prev_numerator / prev_denominator))
 
 
 if __name__ == '__main__':
