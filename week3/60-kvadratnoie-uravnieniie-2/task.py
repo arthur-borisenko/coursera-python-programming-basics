084fed2b63046eb6dd4e7a528c9356c8bd7221f2
Art <artour.borisenko@ya.ru>
2024-05-03 16:00:44 +0300

    refactored

--- week3/60-kvadratnoie-uravnieniie-2/task.py
+++ week3/60-kvadratnoie-uravnieniie-2/task.py
@@ -1,51 +1,47 @@
 import math
+from enum import Enum
 
 
-class rootCounts:
+class rootCounts(Enum):
     NaN = "3"
     Two = "2"
     One = "1"
     Zero = "0"
 
 
-def if_n_is_integer_convert_to_int(n):
+def safe_to_int(n):
     if n.is_integer():
         return int(n)
     else:
         return n
 
 
-def calculate_roots(a, b, c):
-    D = b ** 2 - 4 * a * c
-    if D == 0:
-        x = if_n_is_integer_convert_to_int(-b / (2 * a))
-        return rootCounts.One, str(x)
-    elif D > 0:
-        x = if_n_is_integer_convert_to_int(
-            (-b + math.sqrt(D)) / (2 * a))
-        y = if_n_is_integer_convert_to_int(
-            (-b - math.sqrt(D)) / (2 * a))
-        return rootCounts.Two, str(min(x, y)), str(max(x, y))
+def solve_quadratic_equation(a, b, c):
+    if a == 0:
+        if b == 0 and c == 0:
+            return rootCounts.NaN.value
+        elif b == 0:
+            return rootCounts.Zero.value
+        else:
+            return rootCounts.One.value, str(
+                safe_to_int((-c) / b))
     else:
-        return rootCounts.Zero
-
-
-def calculate_root_when_a_is_zero(b, c):
-    if b == 0 and c == 0:
-        return rootCounts.NaN
-    elif b == 0:
-        return rootCounts.Zero
-    else:
-        return rootCounts.One, str(
-            if_n_is_integer_convert_to_int((-c) / b))
+        D = b ** 2 - 4 * a * c
+        if D == 0:
+            x = safe_to_int(-b / (2 * a))
+            return rootCounts.One.value, str(x)
+        elif D > 0:
+            x = safe_to_int((-b + math.sqrt(D)) / (2 * a))
+            y = safe_to_int((-b - math.sqrt(D)) / (2 * a))
+            return rootCounts.Two.value, min(x, y), max(x, y)
+        else:
+            return rootCounts.Zero.value
 
 
 def main():
     a, b, c = float(input()), float(input()), float(input())
-    if a == 0:
-        print(" ".join(calculate_root_when_a_is_zero(b, c)))
-    else:
-        print(" ".join(calculate_roots(a, b, c)))
+    print(
+        " ".join(list(map(str, solve_quadratic_equation(a, b, c)))))
 
 
 if __name__ == '__main__':
