adeacdc648277cd85c8af9475e1dde44925a90d7
Art <artour.borisenko@ya.ru>
2024-05-05 21:43:29 +0300

    no! do not reuse same variables - code is unreadable.
    use a11, a12 and so on. add comment with initial system of equations and a11, a12 .. one - explane new system of equations constraint (that a11 is always >0) done
    use variables from Task!
    
    code unreadable - read 3 variables on first line and other 3 on second done

--- week3/61-sistiema-linieinykh-uravnienii-1/task.py
+++ week3/61-sistiema-linieinykh-uravnienii-1/task.py
@@ -6,12 +6,17 @@ def safe_to_int(n):
 
 
 def main():
-    a, b, c, d, e, f = float(input()), float(input()), float(
-        input()), float(
-        input()), float(input()), float(input())
+    a, b, c = float(input()), float(input()), float(input())
+    d, e, f = float(input()), float(input()), float(input())
+    # a_11 * x + a_12 * y = b_1
+    # a_21 * x + a_22 * y = b_2
+    # (from task)
+    # a_11 is not zero,because system has exactly one solution
     if a == 0:
-        a, b, c, d, e, f = c, d, a, b, f, e
-    y = (f - e * c / a) / (d - c * b / a)
+        a_11, a_12, a_21, a_22, b_1, b_2 = c, d, a, b, f, e
+    else:
+        a_11, a_12, a_21, a_22, b_1, b_2 = a, b, c, d, e, f
+    y = (b_2 - b_1 * a_21 / a_11) / (a_22 - a_21 * a_12 / a_11)
     x = (e - b * y) / a
     print(safe_to_int(x), safe_to_int(y))
 
