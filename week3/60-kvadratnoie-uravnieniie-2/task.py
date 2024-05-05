e864cb3d385cfca1e4acdbc796815dfb5a377936
Art <artour.borisenko@ya.ru>
2024-05-05 22:02:11 +0300

    all done

--- week3/60-kvadratnoie-uravnieniie-2/task.py
+++ week3/60-kvadratnoie-uravnieniie-2/task.py
@@ -2,46 +2,49 @@ import math
 from enum import Enum
 
 
-class rootCounts(Enum):
-    NaN = "3"
-    Two = "2"
-    One = "1"
-    Zero = "0"
+class RootCounts(Enum):
+    NaN = 3
+    Two = 2
+    One = 1
+    Zero = 0
 
 
 def safe_to_int(n):
-    if n.is_integer():
+    if hasattr(n, "is_integer") and n.is_integer():
         return int(n)
     else:
         return n
 
 
-def solve_quadratic_equation(a, b, c):
+def safe_solve_quadratic_equation(a, b, c):
     if a == 0:
         if b == 0 and c == 0:
-            return rootCounts.NaN.value
+            return [RootCounts.NaN.value]
         elif b == 0:
-            return rootCounts.Zero.value
+            return [RootCounts.Zero.value]
         else:
-            return rootCounts.One.value, str(
-                safe_to_int((-c) / b))
+            return [RootCounts.One.value, safe_to_int((-c) / b)]
     else:
-        D = b ** 2 - 4 * a * c
-        if D == 0:
-            x = safe_to_int(-b / (2 * a))
-            return rootCounts.One.value, str(x)
-        elif D > 0:
-            x = safe_to_int((-b + math.sqrt(D)) / (2 * a))
-            y = safe_to_int((-b - math.sqrt(D)) / (2 * a))
-            return rootCounts.Two.value, min(x, y), max(x, y)
-        else:
-            return rootCounts.Zero.value
+        return solve_quadratic_equation(a, b, c)
+
+
+def solve_quadratic_equation(a, b, c):
+    D = b ** 2 - 4 * a * c
+    if D == 0:
+        x = safe_to_int(-b / (2 * a))
+        return [RootCounts.One.value, x]
+    elif D > 0:
+        x = safe_to_int((-b + math.sqrt(D)) / (2 * a))
+        y = safe_to_int((-b - math.sqrt(D)) / (2 * a))
+        return [RootCounts.Two.value, min(x, y), max(x, y)]
+    else:
+        return [RootCounts.Zero.value]
 
 
 def main():
     a, b, c = float(input()), float(input()), float(input())
-    print(
-        " ".join(list(map(str, solve_quadratic_equation(a, b, c)))))
+    result = safe_solve_quadratic_equation(a, b, c)
+    print(" ".join(map(str, map(safe_to_int, result))))
 
 
 if __name__ == '__main__':
