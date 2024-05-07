0e15bbb9faa4c0af5f63a8a9874c4e390f898f73
Art <artour.borisenko@ya.ru>
2024-05-07 22:23:51 +0300

    re-done

--- week3/61-sistiema-linieinykh-uravnienii-1/task.py
+++ week3/61-sistiema-linieinykh-uravnienii-1/task.py
@@ -5,19 +5,45 @@ def safe_to_int(n):
         return n
 
 
+def solve_linear_equation_system(a_matrix, b_vector):
+    """
+    in task, we have linear equation system,
+    that has exactly one solution:
+
+    ax+by=e
+    cx+dy=f
+
+    <=>
+    a_00*x+a_01*y=b_0
+    a_10*x+a_11*y=b_1
+
+    where we choose non-null x coefficient and name it a_00.
+    Such non-null coefficient exists,
+    because system has exactly one solution
+
+    :param a_matrix: [[a, b], [c, d]] - equation system coefficients
+    :param b_vector: [e, f] -- constant terms
+    :return: x, y - solution
+    """
+
+    (a, b), (c, d) = a_matrix[0], a_matrix[1]
+    (e, f) = b_vector
+    if a != 0:
+        a_00, a_01, b_0 = a, b, e
+        a_10, a_11, b_1 = c, d, f
+
+    else:
+        a_10, a_11, b_1 = c, d, f
+        a_00, a_01, b_0 = a, b, e
+    y = (b_1 - b_0 * a_10 / a_00) / (a_11 - a_10 * a_01 / a_00)
+    x = (e - b * y) / a
+    return x, y
+
+
 def main():
     a, b, c = float(input()), float(input()), float(input())
     d, e, f = float(input()), float(input()), float(input())
-    # a_11 * x + a_12 * y = b_1
-    # a_21 * x + a_22 * y = b_2
-    # (from task)
-    # a_11 is not zero,because system has exactly one solution
-    if a == 0:
-        a_11, a_12, a_21, a_22, b_1, b_2 = c, d, a, b, f, e
-    else:
-        a_11, a_12, a_21, a_22, b_1, b_2 = a, b, c, d, e, f
-    y = (b_2 - b_1 * a_21 / a_11) / (a_22 - a_21 * a_12 / a_11)
-    x = (e - b * y) / a
+    x, y = solve_linear_equation_system([[a, b], [c, d]], [e, f])
     print(safe_to_int(x), safe_to_int(y))
 
 
