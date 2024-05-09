0310a57f87971cb6b5788b58c411df5b3dbdbab4
Art <artour.borisenko@ya.ru>
2024-05-09 18:48:21 +0300

    done

--- week3/70-zamiena-podstroki/task.py
+++ week3/70-zamiena-podstroki/task.py
@@ -1,10 +1,6 @@
-TO_PATTERN = "one"
-FROM_PATTERN = "1"
-
-
 def main():
     s = input()
-    print(s.replace(FROM_PATTERN, TO_PATTERN))
+    print(s.replace("1", "one"))
 
 
 if __name__ == "__main__":
