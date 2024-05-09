7667c727a4db3149cbe486e0b0160fa287dc459f
Art <artour.borisenko@ya.ru>
2024-05-09 13:36:32 +0300

    ...

--- week3/67-vtoroie-vkhozhdieniie/task.py
+++ week3/67-vtoroie-vkhozhdieniie/task.py
@@ -1,11 +1,12 @@
 def main():
+    PATTERN = "f"
     s = input()
-    first = s.find("f")
+    first = s.find(PATTERN)
     if first == -1:
         print(-2)
     else:
-        if s[first + 1:].find("f") != -1:
-            print(s[first + 1:].find("f") + len(s[:first + 1]))
+        if s[first + 1:].find(PATTERN) != -1:
+            print(s[first + 1:].find(PATTERN) + len(s[:first + 1]))
         else:
             print(-1)
 
