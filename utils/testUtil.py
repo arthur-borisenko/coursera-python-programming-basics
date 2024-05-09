dcf85ea768207d0c8d7250fc27da38ab97055f0e
Art <artour.borisenko@ya.ru>
2024-05-09 16:50:56 +0300

    remove fileTest from testUtil because it won't work

--- utils/testUtil.py
+++ utils/testUtil.py
@@ -1,4 +1,4 @@
-import sys,os
+import sys, os
 from io import StringIO
 from unittest import mock
 
@@ -11,16 +11,3 @@ def mockAndRun(input_lines: list, method):
         sys.stdout = old_stdout
         value = capturedStdOut.getvalue()
         return value
-
-
-def fileTest(input_file_name, output_file_name, input_text, method):
-    if not os.path.exists("input.txt"):
-        open(input_file_name, "x").close()
-    input_file = open(input_file_name, "w")
-    input_file.write(input_text)
-    if not os.path.exists("input.txt"):
-        open(output_file_name, "x").close()
-    method()
-    f = open(output_file_name, "r")
-    output = f.read()
-    return output
