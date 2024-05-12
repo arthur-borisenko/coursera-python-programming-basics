30fdabb93eb5d52634f4932cb6d9fa9083ee0cf1
Art <artour.borisenko@ya.ru>
2024-05-12 23:00:57 +0300

    done

--- week5/05-zamiechatiel-nyie-chisla-1/test.py
+++ week5/05-zamiechatiel-nyie-chisla-1/test.py
@@ -1,8 +1,11 @@
 import unittest
 from os import system
-
+from utils.testUtil import mockAndRun
+from task import main
 
 class Test(unittest.TestCase):
+    def test_case1(self):
+        self.assertEqual(mockAndRun([],main),"36\n")
     def test_pep8(self):
         value = system("python -m pycodestyle --first task.py")
         print(value)
