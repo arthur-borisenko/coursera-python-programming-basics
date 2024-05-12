461a84d5362a5dcceb61b0297bce4c88771f9f98
Art <artour.borisenko@ya.ru>
2024-05-12 21:57:40 +0300

    done

--- week5/03-flaghi/task.py
+++ week5/03-flaghi/task.py
@@ -4,16 +4,19 @@ FLAG_LINES_TEMPLATE = ["+___ ",
                        "|    "]
 
 
+def safe_format(s, *args):
+    args = list(map(str, args))
+    args_count = min(s.count("{"), s.count("}"))
+    return s.format(*args[:args_count])
+
+
 def main():
     n = int(input())
     result_lines = ["", "", "", ""]
     for i in range(n):
         for j in range(len(FLAG_LINES_TEMPLATE)):
-            if j == 1:
-                result_lines[j] += FLAG_LINES_TEMPLATE[j].format(
-                    i + 1)
-            else:
-                result_lines[j] += FLAG_LINES_TEMPLATE[j]
+            result_lines[j] += safe_format(FLAG_LINES_TEMPLATE[j],
+                                           i + 1)
     print("\n".join(result_lines))
 
 
