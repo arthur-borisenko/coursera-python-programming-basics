dd64a59283cf2790577414793bb8b1d84ad19895
arthur-borisenko <66972198+arthur-borisenko@users.noreply.github.com>
2024-05-16 23:37:27 +0300

    task 11 solved (#81)
    * done

---
+++ week5/11-naimien-shii-polozhitiel-nyi/test.py
@@ -0,0 +1,26 @@
+import unittest
+from os import system
+from task import main
+from utils.testUtil import mockAndRun
+
+
+class Test(unittest.TestCase):
+    def test_case1(self):
+        value = mockAndRun("""5 -4 3 -2 1""".splitlines(), main)
+        expected_result = """1
+"""
+        self.assertEqual(expected_result, value)
+    def test_case2(self):
+        value = mockAndRun("""10 5 0 -5 -10""".splitlines(), main)
+        expected_result = """5
+"""
+        self.assertEqual(expected_result, value)
+    def test_case3(self):
+        value = mockAndRun("""-1 -2 -3 -4 100""".splitlines(), main)
+        expected_result = """100
+"""
+        self.assertEqual(expected_result, value)
+    def test_pep8(self):
+        value = system("python -m pycodestyle --first task.py")
+        print(value)
+        self.assertIs(value, 0)
