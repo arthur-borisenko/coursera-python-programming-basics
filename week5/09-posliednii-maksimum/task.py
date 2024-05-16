98ae9437742a54320e031d108eb6b3a11d437e0e
Art <artour.borisenko@ya.ru>
2024-05-16 21:07:55 +0300

    draft

---
+++ week5/09-posliednii-maksimum/task.py
@@ -0,0 +1,7 @@
+def main():
+    input_list=input().split(" ")
+    max_number=None
+
+
+if __name__ == "__main__":
+    main()
---
+++ week5/09-posliednii-maksimum/test.py
@@ -0,0 +1,26 @@
+import unittest
+from os import system
+from task import main
+from utils.testUtil import mockAndRun
+
+
+class Test(unittest.TestCase):
+    def test_case1(self):
+        value = mockAndRun("""1 2 1 2 1""".splitlines(), main)
+        expected_result = """2 3
+"""
+        self.assertEqual(expected_result, value)
+    def test_case2(self):
+        value = mockAndRun("""1 2 3 4 5""".splitlines(), main)
+        expected_result = """5 4
+"""
+        self.assertEqual(expected_result, value)
+    def test_case3(self):
+        value = mockAndRun("""5 4 3 2 1""".splitlines(), main)
+        expected_result = """5 0
+"""
+        self.assertEqual(expected_result, value)
+    def test_pep8(self):
+        value = system("python -m pycodestyle --first task.py")
+        print(value)
+        self.assertIs(value, 0)
