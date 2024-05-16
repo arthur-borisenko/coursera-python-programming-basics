55455ae7181cf165499cb09e2ec3c1c408196441
Art <artour.borisenko@ya.ru>
2024-05-16 21:07:55 +0300

    draft

--- week5/09-posliednii-maksimum/task.py
+++ week5/09-posliednii-maksimum/task.py
@@ -1,13 +1,6 @@
 def main():
-    input_list = input().split(" ")
-    max_number = int(input_list[0])
-    max_last_index = 0
-    for i in range(len(input_list)):
-        number = int(input_list[i])
-        if number >= max_number:
-            max_number = number
-            max_last_index = i
-    print(max_number, max_last_index)
+    input_list=input().split(" ")
+    max_number=None
 
 
 if __name__ == "__main__":
