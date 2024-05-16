8af1e20a36f1dc55a67f7e799db9f408dddd21eb
Art <artour.borisenko@ya.ru>
2024-05-16 22:15:26 +0300

    task 09 fix draft

--- week5/09-posliednii-maksimum/task.py
+++ week5/09-posliednii-maksimum/task.py
@@ -1,13 +1,26 @@
 def main():
     input_list = input().split(" ")
-    max_number = int(input_list[0])
+    print(*get_last_maximum(input_list))
+
+
+def safe_to_int(n):
+    if hasattr(n, "is_integer") and n.is_integer():
+        return int(n)
+    else:
+        return n
+
+
+def get_last_maximum(input_list):
+    if input_list[0] == "":
+        return []
+    max_number = float(input_list[0])
     max_last_index = 0
     for i in range(len(input_list)):
-        number = int(input_list[i])
+        number = float(input_list[i])
         if number >= max_number:
             max_number = number
             max_last_index = i
-    print(max_number, max_last_index)
+    return safe_to_int(max_number), max_last_index
 
 
 if __name__ == "__main__":
