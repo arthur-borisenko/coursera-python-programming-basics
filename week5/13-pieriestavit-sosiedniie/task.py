4a1765029f41c31b24f0311e5d54d8977e1c2e53
Art <artour.borisenko@ya.ru>
2024-05-20 23:13:39 +0300

    task 13 remove del

--- week5/13-pieriestavit-sosiedniie/task.py
+++ week5/13-pieriestavit-sosiedniie/task.py
@@ -1,14 +1,8 @@
 def main():
     input_list = input().split()
-    last_el = None
-    if len(input_list) % 2 != 0:
-        last_el = int(input_list[-1])
-        del input_list[-1]
-    for i in range(0, len(input_list), 2):
+    for i in range(0, len(input_list)-1, 2):
         (input_list[i], input_list[i + 1]) = (
             input_list[i + 1], input_list[i])
-    if last_el is not None:
-        input_list.append(last_el)
     print(*input_list)
 
 
