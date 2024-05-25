8eb26ffd95e0e83168265ba8be958fda4940508f
arthur-borisenko <66972198+arthur-borisenko@users.noreply.github.com>
2024-05-25 10:35:56 +0300

    task 01 solved (#94)
    done

---
+++ week7/01-kolichiestvo-razlichnykh-chisiel/test.py
@@ -0,0 +1,26 @@
+import unittest
+from os import system
+from task import main
+from utils.testUtil import mockAndRun
+
+
+class Test(unittest.TestCase):
+    def test_case1(self):
+        value = mockAndRun("""1 2 3 2 1""".splitlines(), main)
+        expected_result = """3
+"""
+        self.assertEqual(expected_result, value)
+    def test_case2(self):
+        value = mockAndRun("""1 2 3 4 5 6 7 8 9 10""".splitlines(), main)
+        expected_result = """10
+"""
+        self.assertEqual(expected_result, value)
+    def test_case3(self):
+        value = mockAndRun("""1 2 3 4 5 1 2 1 2 7 3""".splitlines(), main)
+        expected_result = """6
+"""
+        self.assertEqual(expected_result, value)
+    def test_pep8(self):
+        value = system("python -m pycodestyle --first task.py")
+        print(value)
+        self.assertIs(value, 0)
