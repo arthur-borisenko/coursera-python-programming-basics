708bcab8cfe7481f3b76373c0be16fb05c8f093c
Art <artour.borisenko@ya.ru>
2024-05-26 20:23:56 +0300

    task 04 solved

---
+++ week7/04-vstriechalos-li-chislo-ran-shie/test.py
@@ -0,0 +1,21 @@
+import unittest
+from os import system
+from task import main
+from utils.testUtil import mockAndRun
+
+
+class Test(unittest.TestCase):
+    def test_case1(self):
+        value = mockAndRun("""1 2 3 2 3 4""".splitlines(), main)
+        expected_result = """NO
+NO
+NO
+YES
+YES
+NO
+"""
+        self.assertEqual(expected_result, value)
+    def test_pep8(self):
+        value = system("python -m pycodestyle --first task.py")
+        print(value)
+        self.assertIs(value, 0)
