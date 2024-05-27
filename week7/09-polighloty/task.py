2634eae21795cb3d337eaa52b47aa0015852b404
Art <artour.borisenko@ya.ru>
2024-05-27 18:50:32 +0300

    task 09 solved

--- week7/09-polighloty/task.py
+++ week7/09-polighloty/task.py
@@ -1,25 +1,22 @@
 def main():
-    with (open("input.txt", encoding="utf-8") as input_file, open(
-            "output.txt", "w", encoding="utf-8") as output_file):
-        all_students_know_set = None
-        at_least_one_student_know_set = set()
-        n = int(input_file.readline())
-        for i in range(n):
-            current_student_know_set = set()
-            m = int(input_file.readline())
-            for j in range(m):
-                current_student_know_set.add(input_file.readline().strip())
-            at_least_one_student_know_set |= current_student_know_set
-            if all_students_know_set is None:
-                all_students_know_set = current_student_know_set
-            else:
-                all_students_know_set = all_students_know_set \
-                                        & current_student_know_set
-        print(len(all_students_know_set), file=output_file)
-        print(*all_students_know_set, sep="\n", file=output_file)
-        print(len(at_least_one_student_know_set), file=output_file)
-        print(*at_least_one_student_know_set, sep="\n",
-              file=output_file)
+    all_students_know_set = None
+    at_least_one_student_know_set = set()
+    n = int(input())
+    for i in range(n):
+        current_student_know_set = set()
+        m = int(input())
+        for j in range(m):
+            current_student_know_set.add(input().strip())
+        at_least_one_student_know_set |= current_student_know_set
+        if all_students_know_set is None:
+            all_students_know_set = current_student_know_set
+        else:
+            all_students_know_set = all_students_know_set \
+                                    & current_student_know_set
+    print(len(all_students_know_set))
+    print(*all_students_know_set, sep="\n")
+    print(len(at_least_one_student_know_set))
+    print(*at_least_one_student_know_set, sep="\n")
 
 
 if __name__ == "__main__":
--- week7/09-polighloty/test.py
+++ week7/09-polighloty/test.py
@@ -1,31 +1,12 @@
-import os.path
-from os import system
 import unittest
-import task
+from os import system
+from task import main
+from utils.testUtil import mockAndRun
 
 
-def file_test(i, m):
-    if not os.path.exists("input.txt"):
-        open("input.txt", "x").close()
-    input_file = open("input.txt", "w")
-    try:
-        input_file.write(i)
-    finally:
-        input_file.close()
-    if not os.path.exists("output.txt"):
-        open("output.txt", "x").close()
-    output_file = open("output.txt", "r")
-    try:
-        m()
-        result = output_file.read()
-    finally:
-        output_file.close()
-    return result
-
-
-class TestCase(unittest.TestCase):
+class Test(unittest.TestCase):
     def test_case1(self):
-        result = file_test("""3
+        result = mockAndRun("""3
 3
 Russian
 English
@@ -34,14 +15,16 @@ Japanese
 Russian
 English
 1
-English""", task.main)
-        value=result.splitlines()
-        a=value[0]
-        b=set(value[1:2])
-        c=value[2]
-        d=set(value[3:])
-        exp_res=("1",{"English"},"3",{"Russian","English","Japanese"})
-        self.assertEqual(exp_res, (a,b,c,d))
+English""".splitlines(), main)
+        value = result.splitlines()
+        a = value[0]
+        b = set(value[1:2])
+        c = value[2]
+        d = set(value[3:])
+        exp_res = (
+        "1", {"English"}, "3", {"Russian", "English", "Japanese"})
+        self.assertEqual(exp_res, (a, b, c, d))
+
     def test_pep8(self):
         value = system("python -m pycodestyle --first task.py")
         print(value)
