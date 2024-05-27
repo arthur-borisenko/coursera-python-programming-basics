3457e8928ba7a073457a7d32ae48aba8cadaba00
Art <artour.borisenko@ya.ru>
2024-05-28 01:09:29 +0300

    task 02 solved + update testUtil to support sys.stdin

--- utils/testUtil.py
+++ utils/testUtil.py
@@ -1,10 +1,17 @@
+import io
 import sys, os
 from io import StringIO
 from unittest import mock
 
 
 def mockAndRun(input_lines: list, method):
-    with mock.patch('builtins.input', side_effect=input_lines):
+    new_stdin = io.TextIOWrapper(
+        io.BytesIO(
+            "\n".join(input_lines)
+            .encode('utf-8')),
+        encoding='utf-8')
+    with mock.patch(
+            'sys.stdin', new=new_stdin):
         old_stdout = sys.stdout
         sys.stdout = capturedStdOut = StringIO()
         method()
