6743ca50a5e379bd09f5badbd24a72a064fbb9c1
arthur-borisenko <66972198+arthur-borisenko@users.noreply.github.com>
2024-05-28 15:54:15 +0300

    task 04 solved (#111)
    done

---
+++ week8/04-nol-ili-nie-nol/task.py
@@ -0,0 +1,12 @@
+print(
+    any(
+        map(
+            lambda el: int(input()) == 0,
+            range(
+                int(
+                    input()
+                )
+            )
+        )
+    )
+)
